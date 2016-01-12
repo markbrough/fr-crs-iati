55fc74311f8747808a1964a8afb46b82f484715f
Mark Brough <mark.brough@gmail.com>
2016-01-12 19:08:14 +0100

    Make conversion for DAECT spreadsheets

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ from lxml import etree
 from lxml.etree import *
 from lib import xlsx_to_csv as xtc
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import unicodecsv
 import unicodedata, re
 
@@ -45,6 +45,27 @@ def getCountriesRegions():
         cr[row['dac_country_code']] = row
     return cr
 
+def getCodesCountries():
+    csvfile = open('lib/countries-regions.csv', 'r')
+    csv = unicodecsv.DictReader(csvfile)
+    cr = {}
+    for row in csv:
+        cr[row['country_name_FR']] = row
+    return cr
+
+codesCountries = getCodesCountries()
+
+def getStatus(value):
+    if value == "En cours":
+        return ("2", u"En cours")
+    return ("3", u"Terminé")
+
+def getCodeFromCountry(value):
+    country = codesCountries.get(value)
+    if not country:
+        return "998"
+    return country.get("dac_country_code")
+
 def newE(element_name, activity):
     element = Element(element_name)
     activity.append(element)
@@ -53,13 +74,24 @@ def newE(element_name, activity):
 def makeISO(date):
     return datetime.strptime(date, "%d/%m/%Y").date().isoformat()
 
+def makeISOYear(date):
+    try:
+        return datetime.strptime(date, "%Y").date().isoformat()
+    except ValueError:
+        return "2015-01-01"
+
+def makeISOYearEnd(date):
+    try:
+        year = str(int(date)+1)
+        yeardate = datetime.strptime(year, "%Y").date()
+        yeardate = yeardate - timedelta(days=1)
+        return yeardate.isoformat()
+    except ValueError:
+        return "2015-12-31"
+
 def make_identifier(activity, row, i):
     iati_identifier = newE("iati-identifier", activity)
-    extending_org = getD(row, "Agence exécutive")
-    project_id = getD(row, "N° de projet du donneur")
-    if (project_id == ""):
-        project_id = getD(row, "Rubrique MAE") + "-"+str(i)
-    iati_identifier.text = "FR-"+extending_org+"-"+project_id
+    iati_identifier.text = "FR-DAECT-%s" % i
     return iati_identifier
 
 def write_project(doc, row, cr, i):
@@ -71,10 +103,14 @@ def write_project(doc, row, cr, i):
     doc.append(activity)
 
     title = newE("title", activity)
-    title.text = getD(row, "Description succinte / titre du projet")
+    title.text = getD(row, "Intitulé du projet")
 
     description = newE("description", activity)
-    description.text = getD(row, "Description")
+    description.text = getD(row, "Description détaillée")
+
+    reporting_org = newE("reporting-org", activity)
+    reporting_org.set("ref", "FR")
+    reporting_org.text = "France"
 
     funding_org = newE("participating-org", activity)
     funding_org.set("role", "Funding")
@@ -83,46 +119,54 @@ def write_project(doc, row, cr, i):
 
     extending_org = newE("participating-org", activity)
     extending_org.set("role", "Extending")
-    extending_org.set("ref", "FR-"+getD(row, "Agence exécutive"))
-    extending_org.text = getD(row, "Agence exécutive (nom)")
+    extending_org.set("ref", "FR-99")
+    extending_org.text = getD(row, "Collectivité")
 
     implementing_org = newE("participating-org", activity)
     implementing_org.set("role", "Implementing")
-    implementing_org.set("ref", getD(row, "Canal d'acheminement (code)"))
-    implementing_org.text = getD(row, "canal")
+    implementing_org.text = getD(row, "Partenaire")
 
     iati_identifier = make_identifier(activity, row, i)
 
     collab_type = newE("collaboration-type", activity)
-    collab_type.set("code", getD(row, "Bi/Multi"))
-    collab_type.text = getD(row, """Bi/Multi 
-(nom)""")
+    collab_type.set("code", "1")
+    collab_type.text = ut("Bilatéral")
+
+    planned_start = newE("activity-date", activity)
+    planned_start.set("type", "start-planned")
+    planned_start.set("iso-date", makeISOYear(getD(row, "Année de début")))
 
     flow_type = newE("default-flow-type", activity)
-    flow_type.set("code", getD(row, "Type de ressource"))
-    flow_type.text = getD(row, "Type de ressource (nom)")
+    flow_type.set("code", "10")
+    flow_type.text = ut("APD (aide publique au développement)")
 
     finance_type = newE("default-finance-type", activity)
-    finance_type.set("code", getD(row, "Type de financement"))
-    finance_type.text = getD(row, """Type de financement
-(nom)""")
+    finance_type.set("code", "110")
+    finance_type.text = ut("Don sauf réorganisation de la dette")
 
     aid_type = newE("default-aid-type", activity)
-    aid_type.set("code", getD(row, "Type d'aide"))
-    aid_type.text = getD(row, """Type d'aide
-(nom)""")
+    aid_type.set("code", "C01")
+    aid_type.text = ut("Interventions de type projet")
 
     tied_status = newE("default-tied-status", activity)
     tied_status.set("code", "5")
     tied_status.text = ut("Non lié")
 
-    sector = newE("sector", activity)
-    sector.set("code", getD(row, "Secteur (code)"))
-    sector.text = getD(row, """Secteur
-(nom)""")
+    theStatus = getStatus(getD(row, "Statut"))
+    status = newE("activity-status", activity)
+    status.set("code", theStatus[0])
+    status.text = theStatus[1]
 
-    country_region = getD(row, """Pays bénéficiaire du CAD
-(code)""")
+    sector = newE("sector", activity)
+    sector.set("vocabulary", "RO")
+    sector.text = getD(row, """Thématique""")
+
+    sector = newE("sector", activity)
+    sector.set("vocabulary", "DAC")
+    sector.set("code", "43010")
+    sector.text = "Aide plurisectorielle"
+
+    country_region = getCodeFromCountry(getD(row, "Pays"))
     
     tcr = cr.get(country_region)
     if (tcr and tcr['type'] =='Country'):
@@ -135,34 +179,69 @@ def write_project(doc, row, cr, i):
         region.set('code', tcr['dac_region_code'])
         region.text = tcr['dac_region_name']
 
-    if getD(row, "Montant versé en millliers  d'euros") != "":
+    if getD(row, "Coût total de l’opération") != "0":
+        d_tr = newE("transaction", activity)
+        d_tr_type = newE("transaction-type", d_tr)
+        d_tr_type.set("code", "C")
+        d_tr_type.text = "Engagement"
+        d_tr_date = newE("transaction-date", d_tr)
+        d_tr_date.set("iso-date", makeISOYear(getD(row, "Année de début")))
+        d_tr_value = newE("value", d_tr)
+        d_tr_value.set("currency", "EUR")
+        d_tr_value.set("value-date", makeISOYear(getD(row, "Année de début")))
+        d_tr_value.text = str(float(getD(row, "Coût total de l’opération")))
+        d_tr_description = newE("description", d_tr)
+        d_tr_description.text = u"Année de début du projet; montant total"
+
+    if getD(row, "Cofinancement du MAEDI") != u"0":
         d_tr = newE("transaction", activity)
         d_tr_type = newE("transaction-type", d_tr)
         d_tr_type.set("code", "D")
         d_tr_type.text = "Versement"
         d_tr_date = newE("transaction-date", d_tr)
-        d_tr_date.set("iso-date", "2013-12-31")
+        d_tr_date.set("iso-date", makeISOYearEnd(getD(row, "Année de début")))
         d_tr_value = newE("value", d_tr)
         d_tr_value.set("currency", "EUR")
-        d_tr_value.set("value-date", "2013-12-31")
-        d_tr_value.text = str(float(getD(row, "Montant versé en millliers  d'euros"))*1000)
+        d_tr_value.set("value-date", makeISOYearEnd(getD(row, "Année de début")))
+        d_tr_value.text = str(float(getD(row, "Cofinancement du MAEDI")))
+        d_tr_provider = newE("provider-org", d_tr)
+        d_tr_provider.set("ref", "FR-6")
+        d_tr_provider.text = "MAEDI"
+        d_tr_description = newE("description", d_tr)
+        d_tr_description.text = u"Fin de l'année de début du projet; cofinancement du MAEDI"
 
-    if getD(row, """Montant de l'engagement
-en milliers d'euros""") != "":
-        c_tr = newE("transaction", activity)
-        c_tr_type = newE("transaction-type", c_tr)
-        c_tr_type.set("code", "C")
-        c_tr_type.text = "Engagement"
-        c_tr_date = newE("transaction-date", c_tr)
-        c_tr_date.set("iso-date", makeISO(getD(row, "Date d'engagement")))
-        c_tr_value = newE("value", c_tr)
-        c_tr_value.set("currency", "EUR")
-        c_tr_value.set("value-date", "2013-12-31")
-        c_tr_value.text = str(float(getD(row, """Montant de l'engagement
-en milliers d'euros"""))*1000)
+    if getD(row, "Part financière des collectivités") != u"0":
+        d_tr = newE("transaction", activity)
+        d_tr_type = newE("transaction-type", d_tr)
+        d_tr_type.set("code", "D")
+        d_tr_type.text = "Versement"
+        d_tr_date = newE("transaction-date", d_tr)
+        d_tr_date.set("iso-date", makeISOYearEnd(getD(row, "Année de début")))
+        d_tr_value = newE("value", d_tr)
+        d_tr_value.set("currency", "EUR")
+        d_tr_value.set("value-date", makeISOYearEnd(getD(row, "Année de début")))
+        d_tr_value.text = str(float(getD(row, "Part financière des collectivités")))
+        d_tr_provider = newE("provider-org", d_tr)
+        d_tr_provider.set("ref", "FR-99")
+        d_tr_provider.text = getD(row, "Collectivité")
+        d_tr_description = newE("description", d_tr)
+        d_tr_description.text = u"Fin de l'année de début du projet; cofinancement du collectivité"
 
-    no = getD(row, "Nature de l'opération (nom)")
-
+    if getD(row, "Total autre financement") != u"0":
+        d_tr = newE("transaction", activity)
+        d_tr_type = newE("transaction-type", d_tr)
+        d_tr_type.set("code", "D")
+        d_tr_type.text = "Versement"
+        d_tr_date = newE("transaction-date", d_tr)
+        d_tr_date.set("iso-date", makeISOYearEnd(getD(row, "Année de début")))
+        d_tr_value = newE("value", d_tr)
+        d_tr_value.set("currency", "EUR")
+        d_tr_value.set("value-date", makeISOYearEnd(getD(row, "Année de début")))
+        d_tr_value.text = str(float(getD(row, "Total autre financement")))
+        d_tr_provider = newE("provider-org", d_tr)
+        d_tr_provider.text = getD(row, "Autre")
+        d_tr_description = newE("description", d_tr)
+        d_tr_description.text = u"Fin de l'année de début du projet; cofinancement d'autres organisations"
 
 def convert():
     cr = getCountriesRegions()
